f6c33e666aa9b48e7739fcfdd5e85abe6381859a
vonnenaut <danielashcom@gmail.com>
2019-08-19 17:08:22 +0700

    Removed some comments from mutation

--- mutation.js
+++ mutation.js
@@ -1,16 +1,10 @@
 function mutation(arr) {
 	/*  Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
-
-	For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
-
-	The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
-
-	Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
 	*/
 	let arrCopy = [...arr];
 	let searchArr = arrCopy[0].toLowerCase().split('');
 
-	// get array of unique values to search for
+	// get array of unique values to search for, all lowercase
 	let searchTerms = arrCopy[1].toLowerCase().split('').filter(function(el, idx, self) {
 		return self.indexOf(el) === idx;
 	});
