ec1fb03f7d8953687b6cbde9ee8e167eb082fc60
vonnenaut <danielashcom@gmail.com>
2019-08-19 10:28:15 +0700

    Added solution to boowho

--- boowho.js
+++ boowho.js
@@ -1,6 +1,19 @@
 function booWho(bool) {
-  // What is the new fad diet for ghost developers? The Boolean.
-  return bool;
+  /* Check if a value is classified as a boolean primitive. Return true or false. 
+  */
+  let boolCheck = typeof bool;
+  if (typeof bool === "boolean") {
+  	return true;
+  } else {
+  	return false;
+  }
 }
 
-booWho(null);
+console.log(booWho(null));
+console.log(booWho(true));
+console.log(booWho(false));
+console.log(booWho(NaN));
+console.log(booWho(undefined));
+console.log(booWho("a string"));
+console.log(booWho(7));
+console.log(booWho(new Object()));
